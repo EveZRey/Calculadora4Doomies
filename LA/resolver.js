1c9a98b02f93f1ecc622d4b8e2111dc3d178ada8
EveZRey <160101697+EveZRey@users.noreply.github.com>
2026-04-28 19:17:29 -0600

    Add files via upload

---
+++ LA/resolver.js
@@ -0,0 +1,408 @@
+class Fraccion {
+  constructor(numerador, denominador = 1) {
+    if (denominador === 0) throw new Error("División por cero");
+    const signo = denominador < 0 ? -1 : 1;
+    this.num = numerador * signo;
+    this.den = Math.abs(denominador);
+    this.simplificar();
+  }
+
+  sacarMCD(a, b) {
+    a = Math.abs(Math.round(a));
+    b = Math.abs(Math.round(b));
+
+    if (isNaN(a) || isNaN(b)) return 1;
+
+    while (b !== 0) {
+      let temporal = b;
+      b = a % b;
+      a = temporal;
+    }
+    return a;
+  }
+
+  simplificar() {
+    const mcd = this.sacarMCD(this.num, this.den);
+    if (mcd !== 0) {
+      this.num /= mcd;
+      this.den /= mcd;
+    }
+    return this;
+  }
+
+  sumar(otra) {
+    return new Fraccion(
+      this.num * otra.den + otra.num * this.den,
+      this.den * otra.den,
+    );
+  }
+
+  restar(otra) {
+    return new Fraccion(
+      this.num * otra.den - otra.num * this.den,
+      this.den * otra.den,
+    );
+  }
+
+  multiplicar(otra) {
+    return new Fraccion(this.num * otra.num, this.den * otra.den);
+  }
+
+  dividir(otra) {
+    if (otra.num === 0) throw new Error("División por cero");
+    return new Fraccion(this.num * otra.den, this.den * otra.num);
+  }
+
+  pasarString() {
+    if (this.num === 0) return "0";
+    if (this.den === 1) return `${this.num}`;
+    return `${this.num}/${this.den}`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const botonR = document.getElementById("botonResolver");
+  const tabla = document.getElementById("tablaMatriz");
+  let factoresDeterminante = [];
+  let intercambios = 0;
+
+  botonR.addEventListener("click", () => {
+    factoresDeterminante = [];
+    intercambios = 0;
+    const opcion = document.querySelector(
+      'input[name="operacion"]:checked',
+    ).value;
+    const codigoError = matrizError();
+
+    switch (codigoError) {
+      case 3:
+        return alert("Existe elementos no numericos");
+      case 2:
+        return alert("La matriz esta incompleta");
+      case 1:
+        return alert("La matriz debe ser cuadrada");
+      case -1:
+        return alert("No hay matriz que resolver");
+      default:
+        break;
+    }
+
+    const matriz = cargarMatriz();
+    console.log("ESTA ES LA MATRIZ A RESOLVER");
+    console.log(matriz.map((fila) => fila.map((f) => f.pasarString())));
+
+    const matrizResuelta = GaussJordan(matriz);
+    console.log("MATRIZ RESUELTA:");
+    console.table(
+      matrizResuelta.map((fila) => fila.map((f) => f.pasarString())),
+    );
+
+    mostrarResultado(matrizResuelta, opcion);
+    interpretacion(matrizResuelta, opcion);
+
+    function matrizError() {
+      const filas = tabla.rows.length;
+      const columnas = tabla.rows[0].cells.length;
+      let tieneDatos = false;
+      let tieneVacios = false;
+      let tieneBasura = false;
+
+      for (let i = 0; i < filas; i++) {
+        for (let j = 0; j < columnas; j++) {
+          const valor = tabla.rows[i].cells[j]
+            .querySelector("input")
+            .value.trim();
+          if (valor === "") {
+            tieneVacios = true;
+            break;
+          } else {
+            tieneDatos = true;
+            const fraccion = pasarAFraccion(valor);
+            if (Number.isNaN(fraccion.num) || Number.isNaN(fraccion.den)) {
+              console.log("EXISTEN VALORES NO NUMERICOOOOOOS");
+              tieneBasura = true;
+              break;
+            }
+          }
+        }
+      }
+
+      if (!tieneDatos) return -1;
+      if (tieneBasura) return 3;
+      if (tieneVacios) return 2;
+
+      if (opcion !== "sel" && filas !== columnas) return 1;
+
+      return 0;
+    }
+
+    function cargarMatriz() {
+      const matriz = [];
+
+      for (let i = 0; i < tabla.rows.length; i++) {
+        const fila = [];
+        const celdas = tabla.rows[i].cells;
+
+        for (let j = 0; j < celdas.length; j++) {
+          const valor = celdas[j].querySelector("input").value.trim();
+          fila.push(pasarAFraccion(valor));
+        }
+        matriz.push(fila);
+      }
+
+      if (opcion == "inv") {
+        const n = matriz.length;
+        for (let i = 0; i < n; i++) {
+          for (let j = 0; j < n; j++) {
+            matriz[i].push(new Fraccion(i === j ? 1 : 0, 1));
+          }
+        }
+      }
+
+      return matriz;
+    }
+
+    function pasarAFraccion(valor) {
+      if (valor === "") return new Fraccion(NaN, NaN);
+      if (valor.includes("/")) {
+        const partes = valor.split("/");
+        if (partes.length !== 2) return new Fraccion(NaN, NaN);
+
+        const numerador = Number(partes[0]);
+        const denominador = Number(partes[1]);
+
+        if (isNaN(numerador) || isNaN(denominador) || denominador === 0)
+          return new Fraccion(NaN, NaN);
+        return new Fraccion(numerador, denominador);
+      }
+      const num = Number(valor);
+      if (isNaN(num)) return new Fraccion(NaN, NaN);
+      return new Fraccion(num, 1);
+    }
+
+    function interpretacion(matrizResuelta, opcion) {
+      const y = matrizResuelta.length;
+      const x = matrizResuelta[0].length;
+
+      const divInter = document.getElementById("interpretacion");
+      divInter.innerHTML = "";
+
+      const contenedorEstilizado = document.createElement("div");
+      contenedorEstilizado.classList.add("resultado-det");
+
+      let mensaje = "";
+      if (opcion === "sel") {
+        let filaDeCeros = false;
+        let contradiccion = false;
+
+        for (let i = 0; i < y; i++) {
+          let coeficientesCero = true;
+          for (let j = 0; j < x - 1; j++) {
+            if (matrizResuelta[i][j].num !== 0) {
+              coeficientesCero = false;
+              break;
+            }
+          }
+
+          if (coeficientesCero) {
+            if (matrizResuelta[i][x - 1].num !== 0) {
+              contradiccion = true;
+              break;
+            }
+            filaDeCeros = true;
+          }
+        }
+
+        if (contradiccion) {
+          mensaje = "> El SEL <strong>no tiene solución</strong>";
+        } else if (filaDeCeros || y < x - 1) {
+          mensaje = "> El SEL es <strong>L.D.</strong> (Infinitas soluciones).";
+        } else {
+          mensaje = "> El SEL es <strong>L.I.</strong> (Solución única).";
+        }
+      } else if (opcion === "inv") {
+        let tieneIdentidad = true;
+        for (let i = 0; i < y; i++) {
+          for (let j = 0; j < y; j++) {
+            const valor = matrizResuelta[i][j];
+            const esCero = valor.num === 0;
+            const esUno = valor.num === 1 && valor.den === 1;
+
+            if ((i === j && !esUno) || (i !== j && !esCero)) {
+              tieneIdentidad = false;
+              break;
+            }
+          }
+          if (!tieneIdentidad) break;
+        }
+
+        mensaje = tieneIdentidad
+          ? "> Matriz <strong>Inversa</strong>."
+          : "> La matriz <strong>no tiene inversa</strong>.";
+      } else if (opcion === "det") {
+        let tieneFilaCeros = false;
+        for (let i = 0; i < y; i++) {
+          if (matrizResuelta[i][i].num === 0) {
+            tieneFilaCeros = true;
+            break;
+          }
+        }
+
+        let htmlProceso = "";
+
+        if (tieneFilaCeros) {
+          htmlProceso += " * |M<sub>final</sub>| = <strong>0</strong>";
+        } else {
+          console.log("Determinante diferente de 0");
+          let total = new Fraccion(intercambios % 2 === 0 ? 1 : -1, 1);
+          factoresDeterminante.forEach((f) => {
+            total = total.multiplicar(pasarAFraccion(String(f)));
+          });
+          htmlProceso += `= <strong>${total.pasarString()}</strong>`;
+        }
+        mensaje = `> Determinante: ${htmlProceso}`;
+      }
+
+      contenedorEstilizado.innerHTML = mensaje;
+      divInter.appendChild(contenedorEstilizado);
+    }
+  });
+
+  function moverPivote(matriz, j) {
+    const n = matriz.length;
+    let j_max = j;
+
+    const a_valor = (f) => Math.abs(f.num / f.den);
+
+    for (let i = j; i < n; i++) {
+      if (a_valor(matriz[i][j]) > a_valor(matriz[j_max][j])) {
+        j_max = i;
+      }
+    }
+
+    if (j_max !== j) {
+      [matriz[j], matriz[j_max]] = [matriz[j_max], matriz[j]];
+      intercambios++;
+      console.log(`Intercambio: Fila ${j + 1} con Fila ${j_max + 1}`);
+    }
+
+    if (matriz[j][j].num === 0) {
+      console.log("La columna es toda ceros, no hay pivote.");
+      return false;
+    }
+
+    return true;
+  }
+
+  function hacerPivoteUno(matriz, i, j) {
+    const n_columnas = matriz[0].length;
+    const pivote = matriz[i][j];
+
+    if (pivote.num !== 0) {
+      factoresDeterminante.push(pivote.pasarString());
+
+      for (let k = 0; k < n_columnas; k++) {
+        matriz[i][k] = matriz[i][k].dividir(pivote);
+      }
+      return true;
+    } else {
+      console.log("No hay pivote en esta fila.");
+      return false;
+    }
+  }
+
+  function hacerCerosAbajo(matriz, i, j) {
+    const n_filas = matriz.length;
+    const n_columnas = matriz[0].length;
+    const pivoteFila = i;
+
+    for (let mi = pivoteFila + 1; mi < n_filas; mi++) {
+      const factor = matriz[mi][j];
+
+      for (let nj = 0; nj < n_columnas; nj++) {
+        const sustraendo = factor.multiplicar(matriz[pivoteFila][nj]);
+        matriz[mi][nj] = matriz[mi][nj].restar(sustraendo);
+      }
+    }
+  }
+
+  function hacerCerosArriba(matriz, i, j) {
+    const n_columnas = matriz[0].length;
+    const pivoteFila = i;
+
+    for (let mi = 0; mi < pivoteFila; mi++) {
+      const factor = matriz[mi][j];
+
+      for (let nj = 0; nj < n_columnas; nj++) {
+        const sustraendo = factor.multiplicar(matriz[pivoteFila][nj]);
+        matriz[mi][nj] = matriz[mi][nj].restar(sustraendo);
+      }
+    }
+  }
+
+  function GaussJordan(matriz) {
+    const m = matriz.length;
+
+    for (let i = 0; i < m; i++) {
+      let j = i;
+
+      if (moverPivote(matriz, i, j)) {
+        hacerPivoteUno(matriz, i, j);
+        hacerCerosAbajo(matriz, i, j);
+      }
+    }
+
+    for (let i = m - 1; i >= 0; i--) {
+      let j = i;
+      if (matriz[i][j] && matriz[i][j].num !== 0) {
+        hacerCerosArriba(matriz, i, j);
+      }
+    }
+
+    return matriz;
+  }
+
+  function mostrarResultado(matriz, opcion) {
+    if (opcion === "det") {
+      return;
+    }
+    const contenedor = document.getElementById("resultado");
+    contenedor.innerHTML = "";
+
+    const tablaSolucion = document.createElement("table");
+    tablaSolucion.classList.add("tabla-resultado");
+
+    const filas = matriz.length;
+    const columnas = matriz[0].length;
+
+    for (let i = 0; i < filas; i++) {
+      const filaTR = document.createElement("tr");
+
+      for (let j = 0; j < columnas; j++) {
+        const celdaTD = document.createElement("td");
+        const divContenido = document.createElement("div");
+        divContenido.classList.add("celda-resultado");
+
+        if (opcion === "sel" && j === columnas - 2) {
+          celdaTD.classList.add("columna-aumentada");
+        } else if (opcion === "inv" && j === columnas / 2 - 1) {
+          celdaTD.classList.add("columna-aumentada");
+        } else if (opcion === "det") {
+          if (j === 0) {
+            celdaTD.classList.add("columna-det-izquierda");
+          }
+          if (j === columnas - 1) {
+            celdaTD.classList.add("columna-det-derecha");
+          }
+        }
+
+        divContenido.textContent = matriz[i][j].pasarString();
+        celdaTD.appendChild(divContenido);
+        filaTR.appendChild(celdaTD);
+      }
+      tablaSolucion.appendChild(filaTR);
+    }
+
+    contenedor.appendChild(tablaSolucion);
+  }
+});
